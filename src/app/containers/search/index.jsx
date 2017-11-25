49b3ebd2a8ac176756cfef74d55cb54f388e86b7
Renan Azevedo <renansga2@gmail.com>
2017-11-25 03:39:17 -0200

    Add input behavior

--- src/app/containers/search/index.jsx
+++ src/app/containers/search/index.jsx
@@ -1,12 +1,41 @@
 // @flow
 import * as React from 'react';
+import { connect } from 'redux-zero/react';
+import debounce from 'lodash/debounce';
 
-export default class Search extends React.Component<{}> {
+import actions from '../../redux/actions';
+
+type Props = {
+  fetchArtist: (artistName: string) => void,
+  debounceInterval?: number,
+};
+
+export class Search extends React.Component<Props> {
+  static defaultProps = {
+    debounceInterval: 300,
+  };
+
+  debouncedFetchArtist: Function = debounce(
+    this.props.fetchArtist,
+    this.props.debounceInterval,
+  );
+
+  onInputInput = (event: SyntheticInputEvent<HTMLInputElement>) => {
+    this.debouncedFetchArtist(event.currentTarget.value);
+  };
+
   render() {
     return (
       <div className="ArtistSearch">
-        <input type="text" className="input input-search" placeholder="Search" />
+        <input
+          type="text"
+          className="input input-search"
+          placeholder="Search"
+          onInput={this.onInputInput}
+        />
       </div>
     );
   }
 }
+
+export default connect(null, actions)(Search);
--- src/app/containers/search/index.spec.js
+++ src/app/containers/search/index.spec.js
@@ -1,7 +1,55 @@
 import React from 'react';
 
-import Search from './index';
+import { Search } from './index';
 
-it('renders without crashing', () => {
-  expect(shallow(<Search />)).toBeDefined();
+describe('Search', () => {
+  let wrapper;
+
+  const mockActions = () => ({
+    fetchArtist: jest.fn(),
+  });
+
+  const inputHelper = value => {
+    wrapper.find('input').simulate('input', { currentTarget: { value } });
+  };
+
+  it('renders without crashing', () => {
+    wrapper = shallow(<Search {...mockActions()} />);
+    expect(wrapper).toBeDefined();
+  });
+
+  /*
+    Debounce cant be tested on Jest.
+    See https://github.com/lodash/lodash/issues/2893
+   */
+  xit('fetch user info on input event', done => {
+    //const actions = mockActions();
+    //wrapper = shallow(<Search {...actions} debounceInterval={100} />);
+    //
+    //inputHelper('foo');
+    //
+    //setTimeout(() => {
+    //  expect(actions.fetchArtist).not.toHaveBeenCalledWith('foo');
+    //  done();
+    //}, 200);
+  });
+
+  /*
+    Debounce cant be tested on Jest.
+    See https://github.com/lodash/lodash/issues/2893
+   */
+  xit('debounce input event', done => {
+    //  const actions = mockActions();
+    //  wrapper = shallow(<Search {...actions} debounceInterval={10} />);
+    //
+    //  inputHelper('f');
+    //  setTimeout(() => inputHelper('fo'), 5);
+    //  setTimeout(() => inputHelper('foo'), 7);
+    //  setTimeout(() => inputHelper('foo '), 17);
+    //
+    //  setTimeout(() => {
+    //    expect(actions.fetchArtist).toHaveBeenCalledTimes(1);
+    //    done();
+    //  }, 17);
+  });
 });
