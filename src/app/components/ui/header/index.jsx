a7dcb65a624f4dcd99f5ef422087d3fa9bb310b1
Renan Azevedo <renansga2@gmail.com>
2017-11-26 20:06:32 -0200

    Improve header

--- src/app/components/ui/header/index.jsx
+++ src/app/components/ui/header/index.jsx
@@ -1,6 +1,12 @@
 // @flow
 import * as React from 'react';
 
+import './index.css';
+
 export default function Header() {
-  return <div className="Header">ShowMeTheShow</div>;
+  return (
+    <div className="Header">
+      <span className="logo">Show.me</span>
+    </div>
+  );
 }
